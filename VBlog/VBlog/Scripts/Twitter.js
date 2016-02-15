a4555f5988a0e57513c87ec66ef35f0140f2e099
superviktor <victor.pryhidko@gmail.com>
2016-02-16 01:14:22 +0200

    implemented twitter timeline and table of content

--- VBlog/VBlog/Scripts/Twitter.js
+++ VBlog/VBlog/Scripts/Twitter.js
@@ -1,37 +1,38 @@
-﻿$(document).ready(function() {
-    twitter.update('twitter', 4, '#tweets');
-});
+﻿//$(document).ready(function() {
+//    twitter.update('twitter', 3, '#tweets');
+//});
 
-var twitter = {
-    update:function(name, count, element) {
-        $.ajax({
-            url: 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + name + '&count=' + count,
-            dataType: 'jsonp',
-            error: function() {
-                twitter.displayError(element);
-            },
-            success: function(json) {
-                twitter.showTweeets(json, element);
-            }
-        });
-    },
-    displayError:function(element) {
-        $(element).html("Error");
-    },
-    showTweets:function(tweets, element) 
-    {
-        $(element).empty();       
-            var list = $('<ul class="tweetList"></ul>').appendTo(element);
-            $(tweets).each(function (index, tweet)
-            {
-                var time = Date.parse(tweet.created_at.replace('+', 'UTC+'));
-                time = twitter.getRelativeTime(time);
-                var message = tweet.text;
-                list.append('<li class="tweet"><span class="tweetTime"' + time + '</span>' + message + '</li>');
-            });
-    },
-    getRelativeTime:function(time) {
+//var twitter = {
+//    update:function(name, count, element) {
+//        $.ajax({
+//            url: 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + name + '&count=' + count,
+//            dataType: 'jsonp',
+//            error: function() {
+//                twitter.displayError(element);
+//            },
+//            success: function(json) {
+//                twitter.showTweeets(json, element);
+//            }
+//        });
+//    },
+//    displayError:function(element) {
+//        $(element).html("Error");
+//    },
+
+//    showTweets:function(tweets, element) 
+//    {
+//        $(element).empty();       
+//            var list = $('<ul class="tweetList"></ul>').appendTo(element);
+//            $(tweets).each(function (index, tweet)
+//            {
+//                var time = Date.parse(tweet.created_at.replace('+', 'UTC+'));
+//                time = twitter.getRelativeTime(time);
+//                var message = tweet.text;
+//                list.append('<li class="tweet"><span class="tweetTime"' + time + '</span>' + message + '</li>');
+//            });
+//    },
+//    getRelativeTime:function(time) {
         
-    }
-    }
+//    }
+//    }
 
